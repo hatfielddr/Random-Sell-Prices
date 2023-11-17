0635a8b6b6f617c073a623afd8fb614e95f30c03
Drew Hatfield <hatfielddr@gmail.com>
2023-11-17 00:16:03 -0500

    Renamed variables and separated generation code

--- Patches/TimeOfDayPatch.cs
+++ Patches/TimeOfDayPatch.cs
@@ -1,44 +1,55 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace Random_Sell_Prices.Patches
 {
     [HarmonyPatch(typeof(StartOfRound))]
     internal class TimeOfDayPatch
     {
-        static bool hadGoodDay = false;
+        // MATH
+
+        static bool hadPityDay = false;
+        static float generatePrice(int daysUntilDeadline)
+        {
+            float minPercentage = RandomSellPrices.minPercentage.Value;
+            float maxPercentage = RandomSellPrices.maxPercentage.Value;
+
+            if (daysUntilDeadline <= 1 && !hadPityDay)
+            {
+                minPercentage = RandomSellPrices.pityPercentage.Value;
+            }
+
+            if (StartOfRound.Instance.companyBuyingRate >= RandomSellPrices.pityPercentage.Value)
+            {
+                hadPityDay = true;
+            }
+
+            return UnityEngine.Random.Range(minPercentage, maxPercentage);
+        }
+
+        // PATCHES
 
         [HarmonyPatch(typeof(TimeOfDay), nameof(TimeOfDay.SetBuyingRateForDay))]
         [HarmonyPostfix]
         static void setBuyingRatePatch(ref TimeOfDay __instance)
         {
-            float minPercentage = RandomSellPrices.minPercentage.Value;
-            float maxPercentage = RandomSellPrices.maxPercentage.Value;
-
-            if (__instance.daysUntilDeadline <= 1 && !hadGoodDay)
-            {
-                minPercentage = RandomSellPrices.pityPercentage.Value;
-            }
-
-            StartOfRound.Instance.companyBuyingRate = UnityEngine.Random.Range(minPercentage, maxPercentage);
-
-            if (StartOfRound.Instance.companyBuyingRate >= RandomSellPrices.pityPercentage.Value)
-            {
-                hadGoodDay = true;
-            }
+            StartOfRound.Instance.companyBuyingRate = generatePrice(__instance.daysUntilDeadline);
         }
 
         [HarmonyPatch("ResetShip")]
         [HarmonyPostfix]
-        static void resetShipPatch()
+        static void resetShipPatch(ref float ___companyBuyingRate, ref TimeOfDay __instance)
         {
-            hadGoodDay = false;
+            ___companyBuyingRate = generatePrice(__instance.daysUntilDeadline);
+            hadPityDay = false;
         }
 
         [HarmonyPatch(typeof(TimeOfDay), nameof(TimeOfDay.SyncNewProfitQuotaClientRpc))]
         [HarmonyPostfix]
-        static void syncNewProfitQuotaPatch()
+        static void syncNewProfitQuotaClientPatch(ref StartOfRound __instance, ref float ___timeUntilDeadline)
         {
-            hadGoodDay = false;
+            __instance.companyBuyingRate = generatePrice((int)___timeUntilDeadline);
+            hadPityDay = false;
         }
     }
 }
