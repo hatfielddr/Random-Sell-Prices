cdb559ef979bee82b94d83fdf039a30d472470cf
Drew Hatfield <hatfielddr@gmail.com>
2023-11-16 21:18:21 -0500

    squashed a bug with a missing edge case

--- Patches/TimeOfDayPatch.cs
+++ Patches/TimeOfDayPatch.cs
@@ -16,12 +16,12 @@ namespace Random_Sell_Prices.Patches
 
             if (__instance.daysUntilDeadline <= 1 && !hadGoodDay)
             {
-                minPercentage = 0.8f;
+                minPercentage = RandomSellPrices.pityPercentage.Value;
             }
 
             StartOfRound.Instance.companyBuyingRate = UnityEngine.Random.Range(minPercentage, maxPercentage);
 
-            if (StartOfRound.Instance.companyBuyingRate >= 0.8f)
+            if (StartOfRound.Instance.companyBuyingRate >= RandomSellPrices.pityPercentage.Value)
             {
                 hadGoodDay = true;
             }
@@ -30,6 +30,13 @@ namespace Random_Sell_Prices.Patches
         [HarmonyPatch("ResetShip")]
         [HarmonyPostfix]
         static void resetShipPatch()
+        {
+            hadGoodDay = false;
+        }
+
+        [HarmonyPatch(typeof(TimeOfDay), nameof(TimeOfDay.SyncNewProfitQuotaClientRpc))]
+        [HarmonyPostfix]
+        static void syncNewProfitQuotaPatch()
         {
             hadGoodDay = false;
         }
--- Plugin.cs
+++ Plugin.cs
@@ -15,6 +15,7 @@ namespace Random_Sell_Prices
 
         public static ConfigEntry<float> minPercentage;
         public static ConfigEntry<float> maxPercentage;
+        public static ConfigEntry<float> pityPercentage;
 
         private readonly Harmony harmony = new Harmony(modGUID);
 
@@ -32,6 +33,7 @@ namespace Random_Sell_Prices
             // Creating the config file
             minPercentage = Config.Bind("General", "Minimum Selling Percentage", 0.1f, "Minimum random selling price (NOTE: This value is the decimal form of a percentage i.e. 0.1f = 10%.)");
             maxPercentage = Config.Bind("General", "Maximum Selling Percentage", 1.2f, "Maximum random selling price (NOTE: This value is the decimal form of a percentage i.e. 1.2f = 120%.)");
+            pityPercentage = Config.Bind("General", "Pity Selling Percentage", 0.8f, "At least one day per quota period is guaranteed to be at least this price (NOTE: This value is the decimal form of a percentage i.e. 0.8f = 80%.)");
 
             mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
 
